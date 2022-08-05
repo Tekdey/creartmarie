90b6d18665aa9f32e844f1a6427cba433696190b
Tekdey <devw.nbardi@gmail.com>
2022-08-05 20:09:03 +0200

    initial

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./_reset.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
